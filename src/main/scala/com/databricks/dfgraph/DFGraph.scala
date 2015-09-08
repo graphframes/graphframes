371dba760c363763719046625341adda43982dfa
Feynman Liang <fliang@databricks.com>
2015-09-08 12:46:54 -0700

    Adds import export with dirty tests

--- src/main/scala/com/databricks/dfgraph/DFGraph.scala
+++ src/main/scala/com/databricks/dfgraph/DFGraph.scala
@@ -1,18 +1,26 @@
 package com.databricks.dfgraph
 
-import org.apache.spark.graphx.{Edge, Graph, EdgeRDD, VertexRDD}
-import org.apache.spark.sql.{Row, SQLContext, DataFrame}
-
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe.TypeTag
+
+import org.apache.hadoop.fs.Path
+import org.json4s.DefaultFormats
+import org.json4s.JsonAST.JValue
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+
+import org.apache.spark.SparkContext
+import org.apache.spark.graphx.{Edge, EdgeRDD, Graph, VertexRDD}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
 /**
  * Wraps a GraphX graph inside a Spark Dataframe.
  * @tparam VD vertex attribute type
+ * @tparam ED edge attribute type
  */
-class DFGraph[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag](
-    private val vertexDF: DataFrame,
-    private val edgeDF: DataFrame) extends Serializable {
+class DFGraph[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag] private (
+    val vertexDF: DataFrame,
+    val edgeDF: DataFrame) extends Serializable {
   import DFGraph._
   private val _sqlContext: SQLContext = vertexDF.sqlContext
 
@@ -29,27 +37,79 @@ class DFGraph[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag](
     )
     Graph(verts, edges)
   }
+
+  def save(path: String): Unit = DFGraph.save(_sqlContext, vertexDF, edgeDF, path)
 }
 
-private object DFGraph {
-  val VERTEX_ID_COLNAME = "vtxID"
-  val VERTEX_ATTR_COLNAME = "vtxAttr"
-  val EDGE_SRC_ID_COLNAME = "edgeSrcId"
-  val EDGE_TGT_ID_COLNAME = "edgeTgtId"
-  val EDGE_ATTR_COLNAME = "edgeAttr"
+object DFGraph {
+  private val VERTEX_ID_COLNAME = "vtxID"
+  private val VERTEX_ATTR_COLNAME = "vtxAttr"
+  private val EDGE_SRC_ID_COLNAME = "edgeSrcId"
+  private val EDGE_TGT_ID_COLNAME = "edgeTgtId"
+  private val EDGE_ATTR_COLNAME = "edgeAttr"
 
-  def vertexRDDToDf[VD: TypeTag:ClassTag](vtx: VertexRDD[VD]): DataFrame = {
+  val thisFormatVersion = "1.0"
+
+  val thisClassName = "com.databricks.dfgraph.DFGraph"
+
+  def apply[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag](
+      vertexRDD: VertexRDD[VD], edgeRDD: EdgeRDD[ED]): DFGraph[VD, ED] = {
+    new DFGraph[VD, ED](vertexRDDToDf(vertexRDD), edgeRDDToDf(edgeRDD))
+  }
+
+  def apply[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag](graph: Graph[VD, ED]): DFGraph[VD, ED] = {
+    this.apply(graph.vertices, graph.edges)
+  }
+
+  private def vertexRDDToDf[VD: TypeTag:ClassTag](vtx: VertexRDD[VD]): DataFrame = {
     val sqlContext = SQLContext.getOrCreate(vtx.sparkContext)
     sqlContext.createDataFrame(vtx).toDF(VERTEX_ID_COLNAME, VERTEX_ATTR_COLNAME)
   }
 
-  def edgeRDDToDf[ED: TypeTag:ClassTag](edge: EdgeRDD[ED]): DataFrame = {
+  private def edgeRDDToDf[ED: TypeTag:ClassTag](edge: EdgeRDD[ED]): DataFrame = {
     val sqlContext = SQLContext.getOrCreate(edge.sparkContext)
     sqlContext.createDataFrame(edge)
       .toDF(EDGE_SRC_ID_COLNAME, EDGE_TGT_ID_COLNAME, EDGE_ATTR_COLNAME)
   }
 
-  def apply[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag](vertexRDD: VertexRDD[VD], edgeRDD: EdgeRDD[ED]) = {
-    new DFGraph[VD, ED](vertexRDDToDf(vertexRDD), edgeRDDToDf(edgeRDD))
+  def save(sqlContext: SQLContext, vertexDF: DataFrame, edgeDF: DataFrame, path: String): Unit = {
+    val metadata = compact(render
+      (("class" -> thisClassName) ~ ("version" -> thisFormatVersion)))
+    sqlContext.sparkContext.parallelize(Seq(metadata), 1).saveAsTextFile(metadataPath(path))
+
+    val verticesPath = new Path(dataPath(path), "vtxDF").toUri.toString
+    vertexDF.write.parquet(verticesPath)
+
+    val edgesPath = new Path(dataPath(path), "edgeDF").toUri.toString
+    edgeDF.write.parquet(edgesPath)
+  }
+
+  // TODO: store VD, ED in metadata and automatically provide
+  def load[VD:TypeTag:ClassTag, ED:TypeTag:ClassTag](
+      sqlContext: SQLContext, path: String): DFGraph[VD, ED] = {
+    val (loadedClassName, loadedVersion, _) = loadMetadata(sqlContext.sparkContext, path)
+
+    val vertexDataPath = new Path(dataPath(path), "vtxDF").toUri.toString
+    val edgeDataPath = new Path(dataPath(path), "edgeDF").toUri.toString
+
+    val vtxDF = sqlContext.read.parquet(vertexDataPath)
+    val edgeDF = sqlContext.read.parquet(edgeDataPath)
+    new DFGraph[VD, ED](vtxDF, edgeDF)
+  }
+
+  // TODO: reuse mllib.util.modelSaveLoad
+  private def metadataPath(path: String): String = new Path(path, "metadata").toUri.toString
+  private def dataPath(path: String): String = new Path(path, "data").toUri.toString
+
+  /**
+   * Load metadata from the given path.
+   * @return (class name, version, metadata)
+   */
+  private def loadMetadata(sc: SparkContext, path: String): (String, String, JValue) = {
+    implicit val formats = DefaultFormats
+    val metadata = parse(sc.textFile(metadataPath(path)).first())
+    val clazz = (metadata \ "class").extract[String]
+    val version = (metadata \ "version").extract[String]
+    (clazz, version, metadata)
   }
 }
--- src/test/scala/com/databricks/dfgraph/DFGraphSuite.scala
+++ src/test/scala/com/databricks/dfgraph/DFGraphSuite.scala
@@ -1,6 +1,11 @@
 package com.databricks.dfgraph
 
+import java.io.{IOException, File}
+import java.util.UUID
+
+import org.apache.hadoop.util.ShutdownHookManager
 import org.apache.spark.graphx.Graph
+import org.apache.spark.sql.SQLContext
 import org.scalatest.FunSuite
 
 class DFGraphSuite extends FunSuite with LocalSparkContext {
@@ -9,9 +14,9 @@ class DFGraphSuite extends FunSuite with LocalSparkContext {
       val ring = (0L to 100L).zip((1L to 99L) :+ 0L)
       val doubleRing = ring ++ ring
       val graph = Graph.fromEdgeTuples(sc.parallelize(doubleRing), 1)
+
       val vtxRdd = graph.vertices
       val edgeRdd = graph.edges
-
       val graphBuiltFromDFGraph = DFGraph(vtxRdd, edgeRdd).toGraph()
 
       assert(graphBuiltFromDFGraph.vertices.count() === graph.vertices.count())
@@ -19,5 +24,61 @@ class DFGraphSuite extends FunSuite with LocalSparkContext {
     }
   }
 
+  test("import/export") {
+    withSpark { sc =>
+      val ring = (0L to 100L).zip((1L to 99L) :+ 0L)
+      val doubleRing = ring ++ ring
+      val graph = Graph.fromEdgeTuples(sc.parallelize(doubleRing), 1)
+      val dfGraph = DFGraph(graph)
+
+      val tempDir1 = createTempDir()
+      val path1 = tempDir1.toURI.toString
+      dfGraph.save(path1)
+
+      val sqlContext = SQLContext.getOrCreate(sc)
+      val loadedDfGraph = DFGraph.load[Int, Int](sqlContext, path1)
+      assert(loadedDfGraph.vertexDF.collect() === dfGraph.vertexDF.collect())
+      assert(loadedDfGraph.edgeDF.collect() === dfGraph.edgeDF.collect())
+    }
+  }
+
+
+  // TODO: reuse spark.util.Utils
+  // TODO: clean up directory after tests finish
+  /**
+   * Create a temporary directory inside the given parent directory. The directory will be
+   * automatically deleted when the VM shuts down.
+   */
+  private def createTempDir(
+      root: String = System.getProperty("java.io.tmpdir"),
+      namePrefix: String = "spark"): File = {
+    val dir = createDirectory(root, namePrefix)
+//    ShutdownHookManager.registerShutdownDeleteDir(dir)
+    dir
+  }
+
+  /**
+   * Create a directory inside the given parent directory. The directory is guaranteed to be
+   * newly created, and is not marked for automatic deletion.
+   */
+  def createDirectory(root: String, namePrefix: String = "spark"): File = {
+    var attempts = 0
+    val maxAttempts = 10
+    var dir: File = null
+    while (dir == null) {
+      attempts += 1
+      if (attempts > maxAttempts) {
+        throw new IOException("Failed to create a temp directory (under " + root + ") after " +
+          maxAttempts + " attempts!")
+      }
+      try {
+        dir = new File(root, namePrefix + "-" + UUID.randomUUID.toString)
+        if (dir.exists() || !dir.mkdirs()) {
+          dir = null
+        }
+      } catch { case e: SecurityException => dir = null; }
+    }
+    dir.getCanonicalFile
+  }
 }
 
