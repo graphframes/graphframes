92aadce62ceb8c4571687ff12925c203fd8b0979
Feynman Liang <fliang@databricks.com>
2015-09-08 11:26:51 -0700

    Corrects TypeTag/ClassTag but still failing tests

--- src/main/scala/com/databricks/dfgraph/DFGraph.scala
+++ src/main/scala/com/databricks/dfgraph/DFGraph.scala
@@ -3,11 +3,14 @@ package com.databricks.dfgraph
 import org.apache.spark.graphx.{Edge, Graph, EdgeRDD, VertexRDD}
 import org.apache.spark.sql.{Row, SQLContext, DataFrame}
 
+import scala.reflect.ClassTag
+import scala.reflect.runtime.universe.TypeTag
+
 /**
  * Wraps a GraphX graph inside a Spark Dataframe.
  * @tparam VD vertex attribute type
  */
-class DFGraph[VD, ED](
+class DFGraph[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag](
     private val vertexDF: DataFrame,
     private val edgeDF: DataFrame) {
   import DFGraph._
@@ -33,18 +36,18 @@ private object DFGraph {
   val EDGE_TGT_ID_COLNAME = "edgeTgtId"
   val EDGE_ATTR_COLNAME = "edgeAttr"
 
-  def vertexRDDToDf[VD](vtx: VertexRDD[VD]): DataFrame = {
+  def vertexRDDToDf[VD: TypeTag:ClassTag](vtx: VertexRDD[VD]): DataFrame = {
     val sqlContext = SQLContext.getOrCreate(vtx.sparkContext)
     sqlContext.createDataFrame(vtx).toDF(VERTEX_ID_COLNAME, VERTEX_ATTR_COLNAME)
   }
 
-  def edgeRDDToDf[ED](edge: EdgeRDD[ED]): DataFrame = {
+  def edgeRDDToDf[ED: TypeTag:ClassTag](edge: EdgeRDD[ED]): DataFrame = {
     val sqlContext = SQLContext.getOrCreate(edge.sparkContext)
     sqlContext.createDataFrame(edge)
       .toDF(EDGE_SRC_ID_COLNAME, EDGE_TGT_ID_COLNAME, EDGE_ATTR_COLNAME)
   }
 
-  def apply[VD, ED](vertexRDD: VertexRDD[VD], edgeRDD: EdgeRDD[ED]) = {
+  def apply[VD: TypeTag:ClassTag, ED: TypeTag:ClassTag](vertexRDD: VertexRDD[VD], edgeRDD: EdgeRDD[ED]) = {
     new DFGraph[VD, ED](vertexRDDToDf(vertexRDD), edgeRDDToDf(edgeRDD))
   }
 }
