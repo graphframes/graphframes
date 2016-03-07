06acec55b091f320cf9262027f0909ec629b8280
Timothy Hunter <timhunter@databricks.com>
2016-03-07 12:29:54 -0800

    work

--- bin/download_travis_dependencies.sh
+++ bin/download_travis_dependencies.sh
@@ -6,9 +6,9 @@ mkdir -p $HOME/.cache/spark-versions
 filename="$HOME/.cache/spark-versions/spark-1.6.0-bin-hadoop2.6.tgz"
 if [ -f $filename ]; then
 	echo "Downloading file..."
+	echo `which curl`
 	curl http://d3kbcqa49mib13.cloudfront.net/spark-1.6.0-bin-hadoop2.6.tgz > $filename
 	echo "Content of directory:"
 	ls -la $HOME/.cache/spark-versions/*
 fi
 tar xvf $filename --directory $HOME/.cache/spark-versions
-
