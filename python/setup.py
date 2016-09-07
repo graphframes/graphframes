926c17a93737b3578822f46445d9d3e0f7b1a3d9
Andriy Kushnir <a.kushnir@smartweb.com.ua>
2016-09-07 13:59:50 +0300

    Minimal packaging stuff

--- python/setup.py
+++ python/setup.py
@@ -1,2 +1,27 @@
-# Your python setup file. An example can be found at:
-# https://github.com/pypa/sampleproject/blob/master/setup.py
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="graphframes",
+    version="0.2.1",
+    description="GraphFrames: DataFrame-based Graphs",
+    url="https://github.com/graphframes/graphframes",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    license="ASF 2.0",
+    zip_safe=False,
+    keywords=["graphframes", "spark", "graph", "dataframe"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+    ],
+)
