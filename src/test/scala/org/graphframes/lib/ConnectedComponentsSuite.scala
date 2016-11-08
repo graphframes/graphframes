468c6f87bdaa2a1e6ae73756402133aecd6f356c
Xiangrui Meng <meng@databricks.com>
2016-11-07 16:06:37 -0800

    use DecimalType for the sum

--- src/test/scala/org/graphframes/lib/ConnectedComponentsSuite.scala
+++ src/test/scala/org/graphframes/lib/ConnectedComponentsSuite.scala
@@ -22,9 +22,11 @@ import java.io.IOException
 import scala.reflect.ClassTag
 
 import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.functions.{col, lit}
 import org.apache.spark.sql.types.DataTypes
 
 import org.graphframes._
+import org.graphframes.GraphFrame._
 
 class ConnectedComponentsSuite extends SparkFunSuite with GraphFrameTestSparkContext {
 
@@ -68,6 +70,17 @@ class ConnectedComponentsSuite extends SparkFunSuite with GraphFrameTestSparkCon
         .run()
       assertComponents(components, expected)
     }
+  }
+
+  test("really large long IDs") {
+    val max = Long.MaxValue
+    val chain = examples.Graphs.chain(10L)
+    val vertices = chain.vertices.select((lit(max) - col(ID)).as(ID))
+    val edges = chain.edges.select((lit(max) - col(SRC)).as(SRC), (lit(max) - col(DST)).as(DST))
+    val g = GraphFrame(vertices, edges)
+    val components = g.connectedComponents.run()
+    assert(components.count() === 10L)
+    assert(components.groupBy("component").count().count() === 1L)
   }
 
   test("checkpoint interval") {
