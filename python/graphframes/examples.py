77d62435cd3b090bd5558512abd7961a9eacd696
Tuomas Sivula <tsivula@users.noreply.github.com>
2017-03-18 11:54:21 +0200

    Add gridIsingModel into Python example graphs

--- python/graphframes/examples.py
+++ python/graphframes/examples.py
@@ -15,9 +15,12 @@
 # limitations under the License.
 #
 
+import itertools
+from pyspark.sql import functions as sql_functions
 from .graphframe import GraphFrame
 
 __all__ = ['Graphs']
+
 
 class Graphs(object):
     """
@@ -57,3 +60,71 @@ class Graphs(object):
         ], ["src", "dst", "relationship"])
         # Create a GraphFrame
         return GraphFrame(v, e)
+
+    def gridIsingModel(self, n, vStd=1.0, eStd=1.0):
+        """Grid Ising model with random parameters.
+
+        Ising models are probabilistic graphical models over binary variables x,,i,,.
+        Each binary variable x,,i,, corresponds to one vertex, and it may take values -1 or +1.
+        The probability distribution P(X) (over all x,,i,,) is parameterized by vertex factors
+        a,,i,, and edge factors b,,ij,,:
+        {{{
+         P(X) = (1/Z) * exp[ \sum_i a_i x_i + \sum_{ij} b_{ij} x_i x_j ]
+        }}}
+        where Z is the normalization constant (partition function). See
+        [[https://en.wikipedia.org/wiki/Ising_model Wikipedia]] for more information on Ising
+        models.
+
+        Each vertex is parameterized by a single scalar a,,i,,.
+        Each edge is parameterized by a single scalar b,,ij,,.
+
+        :param n: Length of one side of the grid.  The grid will be of size n x n.
+        :param vStd: Standard deviation of normal distribution used to generate vertex factors "a".
+                     Default of 1.0.
+        :param eStd: Standard deviation of normal distribution used to generate edge factors "b".
+                     Default of 1.0.
+        :return: GraphFrame. Vertices have columns "id" and "a". Edges have columns "src", "dst",
+            and "b".  Edges are directed, but they should be treated as undirected in any algorithms
+            run on this model. Vertex IDs are of the form "i,j".  E.g., vertex "1,3" is in the
+            second row and fourth column of the grid.
+        """
+        assert n >= 1,\
+            "Grid graph must have size >= 1, but was given invalid value n = {}".format(n)
+
+        # create coodinates grid
+        coordinates = self._sql.createDataFrame(
+            itertools.product(range(n), range(n)),
+            schema=('i', 'j'))
+
+        # create SQL expression for converting coordinates (i,j) to a string ID "i,j"
+        # avoid Cartesian join due to SPARK-15425: use generator since n should be small
+        toIDudf = sql_functions.udf(lambda i, j: '{},{}'.format(i,j))
+
+        # create the vertex DataFrame
+        # create SQL expression for converting coordinates (i,j) to a string ID "i,j"
+        vIDcol = toIDudf(sql_functions.col('i'), sql_functions.col('j'))
+        # add random parameters generated from a normal distribution
+        seed = 12345
+        vertices = (coordinates.withColumn('id', vIDcol)
+            .withColumn('a', sql_functions.randn(seed) * vStd))
+
+        # create the edge DataFrame
+        # create SQL expression for converting coordinates (i,j+1) and (i+1,j) to string IDs
+        rightIDcol = toIDudf(sql_functions.col('i'), sql_functions.col('j') + 1)
+        downIDcol = toIDudf(sql_functions.col('i') + 1, sql_functions.col('j'))
+        horizontalEdges = (coordinates.filter(sql_functions.col('j') != n - 1)
+            .select(vIDcol.alias('src'), rightIDcol.alias('dst')))
+        verticalEdges = (coordinates.filter(sql_functions.col('i') != n - 1)
+            .select(vIDcol.alias('src'), downIDcol.alias('dst')))
+        allEdges = horizontalEdges.unionAll(verticalEdges)
+        # add random parameters from a normal distribution
+        edges = allEdges.withColumn('b', sql_functions.randn(seed + 1) * eStd)
+
+        # create the GraphFrame
+        g = GraphFrame(vertices, edges)
+
+        # materialize graph as workaround for SPARK-13333
+        g.vertices.cache().count()
+        g.edges.cache().count()
+
+        return g
