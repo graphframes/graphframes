68d81cd0886d62b84eb3a49348e1829f4e370d2f
cgosse <alec@metismachine.com>
2017-10-11 10:26:49 -0400

    added a basic setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,2 +1,6 @@
-# Your python setup file. An example can be found at:
-# https://github.com/pypa/sampleproject/blob/master/setup.py
+from setuptools import setup
+
+setup(name='graphframes',
+      version='0.5.0+metis',
+      packages=['graphframes', 'graphframes.lib']
+)
