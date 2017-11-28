d7ad0b4d0e88e5e986344edbe69294c09b8026c7
cgosse <alec@metismachine.com>
2017-11-28 12:48:48 -0500

    adding suggested info for pypi

---
+++ python/README.md
@@ -0,0 +1,3 @@
+# Graphframes
+This is a dummy readme file.
+The upstream graphframes package does not include install info for the python side of things, so this code accomplishes just that.
--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(name='graphframes',
+      maintainer='Metismachine',  
+      maintainer_email='info@metismachine.com',
+      url='http://metismachine.com',
       version='0.5.9',
       packages=['graphframes', 'graphframes.lib']
 )
