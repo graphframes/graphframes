4fd24015a98c4372c2ac0008ee8db6e968248719
Joseph Bradley <joseph.kurata.bradley@gmail.com>
2018-06-30 09:44:46 -0700

    Fix negation in motif finding: helper methods (#279)
    This picks the helper methods and tests out from #278 for fixing negation in motif finding in #276
    
    * picked helper methods out
    * clarification
    * fix test
    * cleanups per code review

--- src/test/scala/org/graphframes/pattern/PatternSuite.scala
+++ src/test/scala/org/graphframes/pattern/PatternSuite.scala
@@ -32,6 +32,12 @@ class PatternSuite extends SparkFunSuite {
     assert(Pattern.parse("()-[]->(v)") ===
       Seq(AnonymousEdge(AnonymousVertex, NamedVertex("v"))))
 
+    assert(Pattern.parse("()-[e]->()") ===
+      Seq(NamedEdge("e", AnonymousVertex, AnonymousVertex)))
+
+    assert(Pattern.parse("(u)-[e]->(u)") ===
+      Seq(NamedEdge("e", NamedVertex("u"), NamedVertex("u"))))
+
     assert(Pattern.parse("(u); ()-[]->(v)") ===
       Seq(NamedVertex("u"), AnonymousEdge(AnonymousVertex, NamedVertex("v"))))
 
@@ -54,9 +60,95 @@ class PatternSuite extends SparkFunSuite {
         Pattern.parse("->(a)")
       }
     }
+    withClue("Failed to catch parse error with negated vertex") {
+      intercept[InvalidParseException] {
+        Pattern.parse("!(a)")
+      }
+    }
+    withClue("Failed to catch parse error with negated named edge") {
+      val msg = intercept[InvalidParseException] {
+        Pattern.parse("!(a)-[ab]->(b)")
+      }
+      assert(msg.getMessage.contains("does not support negated named edges"))
+    }
+    withClue("Failed to catch parse error with negated named edge") {
+      val msg = intercept[InvalidParseException] {
+        Pattern.parse("!()-[ab]->()")
+      }
+      assert(msg.getMessage.contains("does not support negated named edges"))
+    }
+    withClue("Failed to catch parse error with double negative") {
+      intercept[InvalidParseException] {
+        Pattern.parse("!!(a)-[]->(b)")
+      }
+    }
+    withClue("Failed to catch parse error with completely anonymous edge ()-[]->()") {
+      intercept[InvalidParseException] {
+        Pattern.parse("()-[]->()")
+      }
+    }
+    withClue("Failed to catch parse error with completely anonymous negated edge !()-[]->()") {
+      intercept[InvalidParseException] {
+        Pattern.parse("!()-[]->()")
+      }
+    }
+    withClue("Failed to catch parse error with reused element name") {
+      intercept[InvalidParseException] {
+        Pattern.parse("(a)-[]->(b); ()-[a]->()")
+      }
+    }
+    withClue("Failed to catch parse error with reused element name") {
+      intercept[InvalidParseException] {
+        Pattern.parse("(a)-[a]->(b)")
+      }
+    }
   }
 
   test("empty pattern should be parsable") {
     Pattern.parse("")
   }
+
+  def testFindNamedVerticesOnlyInNegatedTerms(pattern: String, expected: Seq[String]): Unit = {
+    test(s"findNamedVerticesOnlyInNegatedTerms: $pattern") {
+      val patterns = Pattern.parse(pattern)
+      val result = Pattern.findNamedVerticesOnlyInNegatedTerms(patterns)
+      assert(result === expected)
+    }
+  }
+
+  testFindNamedVerticesOnlyInNegatedTerms(
+    "(u)-[]->(v); (v)-[]->(w); !(u)-[]->(w)",
+    Seq.empty[String])
+
+  testFindNamedVerticesOnlyInNegatedTerms(
+    "(u)-[]->(v); (v)-[]->(w)",
+    Seq.empty[String])
+
+  testFindNamedVerticesOnlyInNegatedTerms(
+    "!(u)-[]->(v)",
+    Seq("u", "v"))
+
+  testFindNamedVerticesOnlyInNegatedTerms(
+    "(u)-[]->(v); (v)-[]->(w); !(a)-[]->(b); !(v)-[]->(c)",
+    Seq("a", "b", "c"))
+
+  def testFindNamedElementsInOrder(pattern: String, expected: Seq[String]): Unit = {
+    test(s"testFindNamedElementsInOrder: $pattern") {
+      val patterns = Pattern.parse(pattern)
+      val result = Pattern.findNamedElementsInOrder(patterns, includeEdges = true)
+      assert(result === expected)
+    }
+  }
+
+  testFindNamedElementsInOrder(
+    "(u)-[]->(v); (v)-[]->(w); !(u)-[]->(w)",
+    Seq("u", "v", "w"))
+
+  testFindNamedElementsInOrder(
+    "(u)-[]->(v); ()-[vw]->()",
+    Seq("u", "v", "vw"))
+
+  testFindNamedElementsInOrder(
+    "(u)-[uv]->(v); (v)-[uv]->(w); !(u)-[]->(w); (x)",
+    Seq("u", "uv", "v", "w", "x"))
 }
