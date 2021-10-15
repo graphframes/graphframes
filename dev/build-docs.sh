1c2310865d950f632b87966c7fc0fd07be682d1c
Xiangrui Meng <meng@databricks.com>
2021-10-15 15:04:14 -0700

    update doc build

--- dev/build-docs.sh
+++ dev/build-docs.sh
@@ -2,15 +2,22 @@
 
 set -euxo pipefail
 
-docker build -t databricks/graphframes .
+if [[ $(uname -sm) == "Darwin arm64" ]]; then
+  # https://github.com/docker/for-mac/issues/5419#issuecomment-834624859
+  PLATFORM_OPT="--platform=linux/arm64"
+else
+  PLATFORM_OPT=
+fi
+
+docker build $PLATFORM_OPT -t graphframes/dev .
 
 # build the docs image
-docker build -t databricks/graphframes-docs docs/
+docker build $PLATFORM_OPT -t graphframes/docs docs/
 
 # build the API docs
 # TODO fix docker on linux to only create files as current user.
-docker run --rm \
+docker run $PLATFORM_OPT --rm \
     -v "$(pwd):/mnt/graphframes" \
     -v "$HOME/.sbt:/root/.sbt" \
     -v "$HOME/.ivy2:/root/.ivy2" \
-    databricks/graphframes-docs bash -i -c dev/build-docs-in-docker.sh
+    graphframes/docs bash -i -c dev/build-docs-in-docker.sh
