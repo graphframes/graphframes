74432f71afbd542f836e92e74fc2f64fa044a320
Russell Jurney <russell.jurney@gmail.com>
2025-02-07 01:18:36 -0800

    Sync'd tutorial with motif.py

--- python/graphframes/tutorials/motif.py
+++ python/graphframes/tutorials/motif.py
@@ -6,41 +6,42 @@
 # Batch Usage: spark-submit --packages graphframes:graphframes:0.8.3-spark3.5-s_2.12 python/graphframes/tutorials/motif.py
 #
 
-import sys
-
-# This is needed for the utils import... need to set this project up as a proper package!
-sys.path.append("python/graphframes/tutorials")
-
-from typing import Optional
 import pyspark.sql.functions as F
-from graphframes import GraphFrame
+from pyspark import SparkContext
 from pyspark.sql import DataFrame, SparkSession
-from utils import three_edge_count, four_edge_count, add_degree, add_type_degree
 
+from graphframes import GraphFrame
 
-#
-# Initialize a SparkSession. You can configre SparkSession via: .config("spark.some.config.option", "some-value")
-#
-
+# Initialize a SparkSession
 spark: SparkSession = (
-    SparkSession.builder.appName("Stack Exchange Motif Analysis")
+    SparkSession.builder.appName("Stack Overflow Motif Analysis")
     # Lets the Id:(Stack Overflow int) and id:(GraphFrames ULID) coexist
     .config("spark.sql.caseSensitive", True).getOrCreate()
 )
-spark.sparkContext.setCheckpointDir("data/checkpoints")
+sc: SparkContext = spark.sparkContext
+sc.setCheckpointDir("/tmp/graphframes-checkpoints")
 
-# Change STACKEXCHANGE_SITE if you download a different stackexchange site
+# Change me if you download a different stackexchange site
 STACKEXCHANGE_SITE = "stats.meta.stackexchange.com"
 BASE_PATH = f"python/graphframes/tutorials/data/{STACKEXCHANGE_SITE}"
 
-
 #
-# Load the nodes from disk and cache. GraphFrames likes nodes/vertices and edges/relatonships to be cached.
+# Load the nodes and edges from disk, repartition, checkpoint [plan got long for some reason] and cache.
 #
 
 # We created these in stackexchange.py from Stack Exchange data dump XML files
 NODES_PATH: str = f"{BASE_PATH}/Nodes.parquet"
-nodes_df: DataFrame = spark.read.parquet(NODES_PATH).cache()
+nodes_df: DataFrame = spark.read.parquet(NODES_PATH)
+
+# Repartition the nodes to give our motif searches parallelism
+nodes_df = nodes_df.repartition(50).checkpoint().cache()
+
+# We created these in stackexchange.py from Stack Exchange data dump XML files
+EDGES_PATH: str = f"{BASE_PATH}/Edges.parquet"
+edges_df: DataFrame = spark.read.parquet(EDGES_PATH)
+
+# Repartition the edges to give our motif searches parallelism
+edges_df = edges_df.repartition(50).checkpoint().cache()
 
 # What kind of nodes we do we have to work with?
 node_counts = (
@@ -53,10 +54,6 @@ node_counts = (
 )
 node_counts.show()
 
-# We created these in stackexchange.py from Stack Exchange data dump XML files
-EDGES_PATH: str = f"{BASE_PATH}/Edges.parquet"
-edges_df: DataFrame = spark.read.parquet(EDGES_PATH).cache()
-
 # What kind of edges do we have to work with?
 edge_counts = (
     edges_df.select("src", "dst", F.col("relationship").alias("Edge Type"))
@@ -68,18 +65,12 @@ edge_counts = (
 )
 edge_counts.show()
 
-#
-# Create the GraphFrame
-#
 g = GraphFrame(nodes_df, edges_df)
 
-# Add the degree to use as a property in the motifs
-g = add_degree(g).cache()
-
+g.vertices.show(10)
 print(f"Node columns: {g.vertices.columns}")
 
-g.vertices.show()
-g.edges.show()
+g.edges.sample(0.0001).show(10)
 
 # Sanity test that all edges have valid ids
 edge_count = g.edges.count()
@@ -96,138 +87,35 @@ assert (
 ), f"Edge count {edge_count} != valid edge count {valid_edge_count}"
 print(f"Edge count: {edge_count:,} == Valid edge count: {valid_edge_count:,}")
 
-#
-# You can find a list of directed motifs here: https://www.nature.com/articles/srep3509
-#
-
 # G4: Continuous Triangles
 paths = g.find("(a)-[e1]->(b); (b)-[e2]->(c); (c)-[e3]->(a)")
-three_edge_count(paths).show()
+
+# Show the first path
+paths.show(3)
+
+graphlet_type_df = paths.select(
+    F.col("a.Type").alias("A_Type"),
+    F.col("e1.relationship").alias("(a)-[e1]->(b)"),
+    F.col("b.Type").alias("B_Type"),
+    F.col("e2.relationship").alias("(b)-[e2]->(c)"),
+    F.col("c.Type").alias("C_Type"),
+    F.col("e3.relationship").alias("(c)-[e3]->(a)"),
+)
+
+graphlet_count_df = (
+    graphlet_type_df.groupby(
+        "A_Type", "(a)-[e1]->(b)", "B_Type", "(b)-[e2]->(c)", "C_Type", "(c)-[e3]->(a)"
+    )
+    .count()
+    .orderBy(F.col("count").desc())
+    # Add a comma formatted column for display
+    .withColumn("count", F.format_number(F.col("count"), 0))
+)
+graphlet_count_df.show()
 
 # G5: Divergent Triangles
 paths = g.find("(a)-[e1]->(b); (a)-[e2]->(c); (c)-[e3]->(b)")
-three_edge_count(paths).show()
 
-# G6: Continuous Path
-paths = g.find("(a)-[e1]->(b); (b)-[e2]->(c); (c)-[e3]->(d)")
-three_edge_count(paths).show()
-
-# G6: Continuous Path Votes with VoteTypes
-graphlet_type_df = paths.select(
-    F.col("a.Type").alias("A_Type"),
-    F.col("a.VoteType").alias("A_VoteType"),
-    F.col("e1.relationship").alias("E_relationship"),
-    F.col("b.Type").alias("B_Type"),
-    F.col("e2.relationship").alias("E2_relationship"),
-    F.col("c.Type").alias("C_Type"),
-    F.col("e3.relationship").alias("E3_relationship"),
-)
-graphlet_count_df = (
-    graphlet_type_df.groupby(
-        "A_Type",
-        "A_VoteType",
-        "E_relationship",
-        "B_Type",
-        "E2_relationship",
-        "C_Type",
-        "E3_relationship",
-    )
-    .count()
-    .filter(F.col("A_Type") == "Vote")
-    .orderBy(F.col("count").desc())
-    # Add a comma formatted column for display
-    .withColumn("count", F.format_number(F.col("count"), 0))
-)
-graphlet_count_df.show()
-
-# +------+----------+--------------+------+---------------+------+---------------+------+
-# |A_Type|A_VoteType|E_relationship|B_Type|E2_relationship|C_Type|E3_relationship| count|
-# +------+----------+--------------+------+---------------+------+---------------+------+
-# |  Vote|    UpVote|       CastFor|  Post|        Answers|  Post|          Links|27,197|
-# |  Vote|    UpVote|       CastFor|  Post|          Links|  Post|          Links|22,947|
-# |  Vote|  DownVote|       CastFor|  Post|        Answers|  Post|          Links| 2,129|
-# |  Vote|  DownVote|       CastFor|  Post|          Links|  Post|          Links| 1,503|
-# |  Vote|   Unknown|       CastFor|  Post|        Answers|  Post|          Links|   523|
-# |  Vote|   Unknown|       CastFor|  Post|          Links|  Post|          Links|   165|
-# |  Vote|      Spam|       CastFor|  Post|          Links|  Post|          Links|    18|
-# |  Vote|    UpVote|       CastFor|  Post|          Links|  Post|        Answers|    17|
-# |  Vote|      Spam|       CastFor|  Post|        Answers|  Post|          Links|    16|
-# |  Vote|Undeletion|       CastFor|  Post|        Answers|  Post|          Links|     1|
-# |  Vote|   Unknown|       CastFor|  Post|          Links|  Post|        Answers|     1|
-# |  Vote|    Reopen|       CastFor|  Post|          Links|  Post|          Links|     1|
-# +------+----------+--------------+------+---------------+------+---------------+------+
-
-# G10: Convergent Triangle
-paths = g.find("(a)-[e1]->(b); (c)-[e2]->(a); (d)-[e3]->(a)")
-three_edge_count(paths).show()
-
-# G10: Convergent Triangle with VoteTypes
-paths = g.find("(a)-[e1]->(b); (c)-[e2]->(a); (d)-[e3]->(a)")
-graphlet_type_df = paths.select(
-    F.col("a.Type").alias("A_Type"),
-    F.col("e1.relationship").alias("E_relationship"),
-    F.col("b.Type").alias("B_Type"),
-    F.col("e2.relationship").alias("E2_relationship"),
-    F.col("c.Type").alias("C_Type"),
-    F.col("c.VoteType").alias("C_VoteType"),
-    F.col("e3.relationship").alias("E3_relationship"),
-    F.col("d.Type").alias("D_Type"),
-    F.col("d.VoteType").alias("D_VoteType"),
-)
-graphlet_count_df = (
-    graphlet_type_df.groupby(
-        "A_Type",
-        "E_relationship",
-        "B_Type",
-        "E2_relationship",
-        "C_Type",
-        "C_VoteType",
-        "E3_relationship",
-        "D_Type",
-        "D_VoteType",
-    )
-    .count()
-    .filter(F.col("C_Type") == "Vote")
-    .filter(F.col("E3_relationship") == "CastFor")
-    .orderBy(F.col("count").desc())
-    # Add a comma formatted column for display
-    .withColumn("count", F.format_number(F.col("count"), 0))
-)
-graphlet_count_df.show()
-
-# +------+--------------+------+---------------+------+----------+---------------+------+----------+-------+
-# |A_Type|E_relationship|B_Type|E2_relationship|C_Type|C_VoteType|E3_relationship|D_Type|D_VoteType|  count|
-# +------+--------------+------+---------------+------+----------+---------------+------+----------+-------+
-# |  Post|       Answers|  Post|        CastFor|  Vote|    UpVote|        CastFor|  Vote|    UpVote|313,807|
-# |  Post|         Links|  Post|        CastFor|  Vote|    UpVote|        CastFor|  Vote|    UpVote|271,944|
-# |  Post|       Answers|  Post|        CastFor|  Vote|    UpVote|        CastFor|  Vote|   Unknown|  8,159|
-# |  Post|       Answers|  Post|        CastFor|  Vote|   Unknown|        CastFor|  Vote|    UpVote|  8,159|
-# |  Post|         Links|  Post|        CastFor|  Vote|    UpVote|        CastFor|  Vote|  DownVote|  6,749|
-# |  Post|         Links|  Post|        CastFor|  Vote|  DownVote|        CastFor|  Vote|    UpVote|  6,749|
-# |  Post|       Answers|  Post|        CastFor|  Vote|  DownVote|        CastFor|  Vote|    UpVote|  6,586|
-# |  Post|       Answers|  Post|        CastFor|  Vote|    UpVote|        CastFor|  Vote|  DownVote|  6,586|
-# |  Post|         Links|  Post|        CastFor|  Vote|  DownVote|        CastFor|  Vote|  DownVote|  4,266|
-# |  Post|       Answers|  Post|        CastFor|  Vote|  DownVote|        CastFor|  Vote|  DownVote|  4,190|
-# |  Post|         Links|  Post|        CastFor|  Vote|    UpVote|        CastFor|  Vote|   Unknown|  1,617|
-# |  Post|         Links|  Post|        CastFor|  Vote|   Unknown|        CastFor|  Vote|    UpVote|  1,617|
-# |  Post|       Answers|  Post|        CastFor|  Vote|   Unknown|        CastFor|  Vote|   Unknown|    919|
-# |  Post|         Links|  Post|        CastFor|  Vote|   Unknown|        CastFor|  Vote|  DownVote|    317|
-# |  Post|         Links|  Post|        CastFor|  Vote|  DownVote|        CastFor|  Vote|   Unknown|    317|
-# |  Post|         Links|  Post|        CastFor|  Vote|   Unknown|        CastFor|  Vote|   Unknown|    239|
-# |  Post|       Answers|  Post|        CastFor|  Vote|   Unknown|        CastFor|  Vote|  DownVote|    113|
-# |  Post|       Answers|  Post|        CastFor|  Vote|  DownVote|        CastFor|  Vote|   Unknown|    113|
-# |  Post|       Answers|  Post|        CastFor|  Vote|    UpVote|        CastFor|  Vote|      Spam|     92|
-# |  Post|       Answers|  Post|        CastFor|  Vote|      Spam|        CastFor|  Vote|    UpVote|     92|
-# +------+--------------+------+---------------+------+----------+---------------+------+----------+-------+
-
-# G14: Cyclic Quadrilaterals
-paths = g.find("(a)-[e1]->(b); (b)-[e2]->(c); (c)-[e3]->(d); (d)-[e4]->(a)")
-four_edge_count(paths).show()
-
-# G22: A triangle with a fourth node pointing at the node with in-degree of 2
-paths = g.find("(a)-[e1]->(b); (a)-[e2]->(c); (c)-[e3]->(b); (d)-[e4]->(b)")
-
-# Visualize the counts by frequency
 graphlet_type_df = paths.select(
     F.col("a.Type").alias("A_Type"),
     F.col("e1.relationship").alias("(a)-[e1]->(b)"),
@@ -235,59 +123,85 @@ graphlet_type_df = paths.select(
     F.col("e2.relationship").alias("(a)-[e2]->(c)"),
     F.col("c.Type").alias("C_Type"),
     F.col("e3.relationship").alias("(c)-[e3]->(b)"),
-    F.col("d.Type").alias("D_Type"),
-    F.col("e4.relationship").alias("(d)-[e4]->(b)"),
 )
+
 graphlet_count_df = (
     graphlet_type_df.groupby(
-        "A_Type",
-        "(a)-[e1]->(b)",
-        "B_Type",
-        "(a)-[e2]->(c)",
-        "C_Type",
-        "(c)-[e3]->(b)",
-        "D_Type",
-        "(d)-[e4]->(b)",
+        "A_Type", "(a)-[e1]->(b)", "B_Type", "(a)-[e2]->(c)", "C_Type", "(c)-[e3]->(b)"
     )
     .count()
     .orderBy(F.col("count").desc())
     # Add a comma formatted column for display
     .withColumn("count", F.format_number(F.col("count"), 0))
 )
-
 graphlet_count_df.show()
 
-# Sort the table across its path to see the logic of the graphlet paths
+# G17: A directed 3-path is a surprisingly diverse graphlet
+paths = g.find("(a)-[e1]->(b); (b)-[e2]->(c); (d)-[e3]->(c)")
+
+# Visualize the four-path by counting instances of paths by node / edge type
+graphlet_type_df = paths.select(
+    F.col("a.Type").alias("A_Type"),
+    F.col("e1.relationship").alias("(a)-[e1]->(b)"),
+    F.col("b.Type").alias("B_Type"),
+    F.col("e2.relationship").alias("(b)-[e2]->(c)"),
+    F.col("c.Type").alias("C_Type"),
+    F.col("e3.relationship").alias("(d)-[e3]->(c)"),
+    F.col("d.Type").alias("D_Type"),
+)
+graphlet_count_df = (
+    graphlet_type_df.groupby(
+        "A_Type",
+        "(a)-[e1]->(b)",
+        "B_Type",
+        "(b)-[e2]->(c)",
+        "C_Type",
+        "(d)-[e3]->(c)",
+        "D_Type",
+    )
+    .count()
+    .orderBy(F.col("count").desc())
+    # Add a comma formatted column for display
+    .withColumn("count", F.format_number(F.col("count"), 0))
+)
+graphlet_count_df.show()
+
 graphlet_count_df.orderBy(
     [
         "A_Type",
         "(a)-[e1]->(b)",
         "B_Type",
-        "(a)-[e2]->(c)",
+        "(b)-[e2]->(c)",
         "C_Type",
-        "(c)-[e3]->(b)",
+        "(d)-[e3]->(c)",
         "D_Type",
-        "(d)-[e4]->(b)",
     ],
-    ascending=True,
-).show()
+    ascending=False,
+).show(104)
 
-# Let's group by and count the votes for self-answered questions
-match_paths = paths.filter(
-    (F.col("a.Type") == "User")
-    & (F.col("e1.relationship") == "Asks")
-    & (F.col("b.Type") == "Post")
-    & (F.col("e2.relationship") == "Answers")
-    & (F.col("c.Type") == "Post")
-    & (F.col("e3.relationship") == "Answers")
+# A user answers an answer that answers a question that links to an answer.
+linked_vote_paths = paths.filter(
+    (F.col("a.Type") == "Vote")
+    & (F.col("e1.relationship") == "CastFor")
+    & (F.col("b.Type") == "Question")
+    & (F.col("e2.relationship") == "Links")
+    & (F.col("c.Type") == "Question")
+    & (F.col("e3.relationship") == "CastFor")
     & (F.col("d.Type") == "Vote")
-    & (F.col("e4.relationship") == "CastFor")
 )
-print(f"Self-answered questions with at leaast one vote, paths: {match_paths.count():,}")
 
-# Now group by all fields but the vote field and count votes
-match_paths.groupBy(
-    "a.id",
-    "b.id",
-    "c.id",
-).count().show()
+# Sanity check the count - it should match the table above
+linked_vote_paths.count()
+
+b_vote_counts = linked_vote_paths.select("a", "b").distinct().groupBy("b").count()
+c_vote_counts = linked_vote_paths.select("c", "d").distinct().groupBy("c").count()
+
+linked_vote_counts = (
+    linked_vote_paths.filter((F.col("a.VoteTypeId") == 2) & (F.col("d.VoteTypeId") == 2))
+    .select("b", "c")
+    .join(b_vote_counts, on="b", how="inner")
+    .withColumnRenamed("count", "b_count")
+    .join(c_vote_counts, on="c", how="inner")
+    .withColumnRenamed("count", "c_count")
+)
+linked_vote_counts.stat.corr("b_count", "c_count")
