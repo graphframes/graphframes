6e44421493c987e09135c809fcc62cd1c217d6c5
Russell Jurney <russell.jurney@gmail.com>
2025-02-21 12:27:54 +0100

    2 of 3: New minimized PR for a Python tutorial module graphframes.tutorial (#518)
    * Minimized the PR to just these files
    
    * Created tutorials dependency group to minimize main bloat
    
    * Make motif.py execute in whole again
    
    * Minor isort format and cleanup of download.py
    
    * Minor isort format and cleanup of utils.py
    
    * Removed case sensitivity from the script - that was confusing people who just pasted or tried to run the code without a new SparkSession.
    
    * motif.py now matches tutorial code, runs and handles case insensitivity.
    
    * 1 of 3: Build a `graphframes` Python package during the build process (#512)
    
    * Converted tests to pytest. Build a Python package. Update requirements.txt and split out requirements-dev.txt. Version bumps.
    
    * Restore Python .gitignore
    
    * Extra newline removed
    
    * Added VERSION file set to 0.8.5
    
    * isort; fiex edgesDF variable name.
    
    * Back out Dockerfile changes
    
    * Back out version change in build.sbt
    
    * Backout changes to config and run-tests
    
    * Back out pytest conversion
    
    * Back out version changes to make nose tests pass
    
    * Remove changes to requirements
    
    * Put nose back in requirements.txt
    
    * Remove version bump to version.sbt
    
    * Remove packages related to testing
    
    * Remove old setup.py / setup.cfg
    
    * New pyproject.toml and poetry.lock
    
    * Short README for Python package, poetry won't allow a ../README.md path
    
    * Remove requirements files in favor of pyproject.toml
    
    * Try to poetrize CI build
    
    * pyspark min 3.4
    
    * Local python README in pyproject.toml
    
    * Trying to remove he working folder to debug scala issue
    
    * Set Python working directory again
    
    * Accidental newline
    
    * Install Python for test...
    
    * Run tests from python/ folder
    
    * Try running tests from python/
    
    * poetry run the unit tests
    
    * poetry run the tests
    
    * Try just using 'python' instead of a path
    
    * poetry run the last line, graphframes.main
    
    * Remove test/ folder from style paths, it doesn't exist
    
    * Remove .vscode
    
    * VERSION back to 0.8.4
    
    * Remove tutorials reference
    
    * VERSION is a Python thing, it belongs in python/
    
    * Include the README.md and LICENSE in the Python package
    
    * Some classifiers for pyproject.toml
    
    * Trying poetry install action instead of manual install
    
    * Removing SPARK_HOME
    
    * Returned SPARK_HOME settings
    
    * Setup a 'graphframes stackexchange' comand.
    
    * Make graphframes.tutorials.motif use a checkpoint dir unique, and from SparkSession.sparkContext. Use click.echo instead of print
    
    * Use spark.sparkContext.setCheckpointDir directly instead of instantiating a SparkContext. print-->click.echo
    
    * Using 'from __future__ import annotations' intsead of List and Tuple
    
    * Now retry three times if we can't connect for any reason in 'graphframes stackexchange' command.

---
+++ python/graphframes/console.py
@@ -0,0 +1,19 @@
+import click
+from graphframes.tutorials import download
+
+
+@click.group()
+def cli():
+    """GraphFrames CLI: a collection of commands for graphframes."""
+    pass
+
+
+cli.add_command(download.stackexchange)
+
+
+def main():
+    cli()
+
+
+if __name__ == "__main__":
+    main()
