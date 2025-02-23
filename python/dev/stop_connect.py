8cb430caf998ab429ee0fda06b33a922dba50a3d
semyonsinchenko <ssinchenko@apache.org>
2025-02-24 00:57:03 +0100

    Fix broken stop-cript

--- python/dev/stop_connect.py
+++ python/dev/stop_connect.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python
 
 
+import os
 import shutil
 import subprocess
 from pathlib import Path
+
+SPARK_VERSION = "3.5.4"
 
 if __name__ == "__main__":
     prj_root = Path(__file__).parent.parent.parent
     scala_root = prj_root.joinpath("graphframes-connect")
     tmp_dir = prj_root.joinpath("tmp")
+    unpackaed_spark_binary = f"spark-{SPARK_VERSION}-bin-hadoop3"
+    spark_home = tmp_dir.joinpath(unpackaed_spark_binary)
+
+    os.chdir(spark_home)
+
     checkpoint_dir = Path("/tmp/GFTestsCheckpointDir")
 
     stop_connect_cmd = ["./sbin/stop-connect-server.sh"]
