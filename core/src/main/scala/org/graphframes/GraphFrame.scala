870997fe9c322902e420525c7bb21afc82edabf6
semyonsinchenko <ssinchenko@apache.org>
2025-09-20 07:22:56 +0200

    Add Rocha–Thatte cycle detection algorithm

--- core/src/main/scala/org/graphframes/GraphFrame.scala
+++ core/src/main/scala/org/graphframes/GraphFrame.scala
@@ -550,6 +550,25 @@ class GraphFrame private (
     }
   }
 
+  /**
+   * Find all cycles in the graph. An implementation of the Rocha–Thatte cycle detection
+   * algorithm.
+   *
+   * Rocha, Rodrigo Caetano, and Bhalchandra D. Thatte. "Distributed cycle detection in
+   * large-scale sparse graphs." Proceedings of Simpósio Brasileiro de Pesquisa Operacional
+   * (SBPO’15) (2015): 1-11.
+   *
+   * Returns a DataFrame with ID and cycles, ID are not unique if there are multiple cycles
+   * starting from this ID. For the case of cycle 1 -> 2 -> 3 -> 1 all the vertices will have the
+   * same cycle! E.g.: 1 -> [1, 2, 3, 1] 2 -> [2, 3, 1, 2] 3 -> [3, 1, 2, 3]
+   *
+   * Deduplication of cycles should be done by the user!
+   *
+   * @return
+   *   an instance of DetectingCycles initialized with the current context
+   */
+  def detectingCycles: DetectingCycles = new DetectingCycles(this)
+
   // ========= Motif finding (private) =========
 
   /**
