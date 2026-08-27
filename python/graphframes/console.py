b5978a9e5daa1e2bc29e09c36c5b31e691cfcb55
Russell Jurney <russell.jurney@gmail.com>
2026-08-26 21:09:28 -0700

    Merge main

--- python/graphframes/console.py
+++ python/graphframes/console.py
@@ -1,14 +1,29 @@
-import click
+import logging
 
+logger = logging.getLogger(__name__)
 
-@click.group()
-def cli():
-    """GraphFrames CLI: a collection of commands for graphframes."""
-    pass
+MISSING_TUTORIALS_EXTRA_MSG = (
+    "The `graphframes` CLI requires the optional `tutorials` dependencies, "
+    "which are not installed. Install them with: "
+    "pip install 'graphframes-py[tutorials]'"
+)
 
 
 def main():
     from graphframes.tutorials import download, neo4j_cli
+
+    try:
+        import click
+
+        from graphframes.tutorials import download
+    except ImportError as err:
+        logger.error("%s (%s)", MISSING_TUTORIALS_EXTRA_MSG, err)
+        raise SystemExit(1) from err
+
+    @click.group()
+    def cli():
+        """GraphFrames CLI: a collection of commands for graphframes."""
+        pass
 
     cli.add_command(download.stackexchange)
     cli.add_command(neo4j_cli.neo4j)
